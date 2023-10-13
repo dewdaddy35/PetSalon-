20abbd1b3052c64c048407d080d0187526ae4478
eric wells <eric.wells41@gmail.com>
2023-10-12 17:38:24 -0700

    salon

--- scripts/register.js
+++ scripts/register.js
@@ -18,6 +18,7 @@
 //console.log(student1.name);
 //console.log(student1.address.zip);
 //console.log(student1.music[1]); display beatles
+
 let salon = {
     name:"The Fashion Pet",
     address:{
@@ -27,30 +28,51 @@ let salon = {
     hours:{
     open:"9:00 am",
     close:"8:00 pm"
-    },
-    pets:[
-        {
-            name:"Scooby",
-            age:70
-        },
-        {
-            name:"Scappy",
-            age:60
-        }
-        
-    ]
+    }
 }
 // create the function
 function displayInfo(){
-document.getElementById("pets").innerHTML=`Welcome to ${salon.name}.....close at ${salon.hours.close}`;
-
+document.getElementById("salon").innerHTML=`Welcome to ${salon.name}.....close at ${salon.hours.close}`;
 }
 //call/run/execute the function
-displayInfo();
-function displayPetNames(){
-    console.log(salon.pets[0].name);
-    console.log(salon.pets[1].name);
 
+displayInfo();
+let pet1= {
+        name:"Scooby",
+        age:"12",
+        gender:"Male",
+        service: "Shampoo and Cut",
+        breed: "German Sheperd" 
 }
-displayPetNames();
+    
+let pet2 =  {
+    name:"Scappy",
+    age:"8",
+    gender:"Female",
+    service:"Nail Clipping",
+    breed:"French Bull Dog",
+    }
+let pet3 = { 
+    name:"Happy",
+    age:"17",
+    gender:"Male",
+    service: "Shampoo, Nail Clipper and Cut",
+    breed: "Pit Bull"
+}
+const pets= [pet1,pet2,pet3];
+
+
+
+
+function displayPetInfo(pet){
+document.getElementById("pet1").innerHTML+=`<p>${pet.name}</p>`;
+}
+
+displayPetInfo(pet1);
+displayPetInfo(pet2);
+displayPetInfo(pet3);
+
+
+
+
 
