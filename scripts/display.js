8fa9d8cdc83a13102ddf993ad71776e04e1e19bf
eric wells <eric.wells41@gmail.com>
2023-10-15 10:38:23 -0700

    salon

---
+++ scripts/display.js
@@ -0,0 +1,15 @@
+function displayCards(){
+    console.log("Displaying");
+    console.log(salon.pets);
+}
+//get the div to display the cards
+
+
+//travel the array
+
+
+
+//create the html template
+
+
+//insert the card into the register.html
--- scripts/register.js
+++ scripts/register.js
@@ -1,24 +1,3 @@
-//objects lieral creating and object
-//let student1={
-//    name:"Eric",
-//    age:28,
-//    country:"usa"
-//    address:{
-//           street:"palm",
-//           number:"22"
-//           zip:"92027"
-//          }
-//music["Rolling Stones", "Beatles", "Queen"]
-//}
-//let student2={
-//    name:"Jack",
-//    age:"29",
-//    country:"USA"
-//}
-//console.log(student1.name);
-//console.log(student1.address.zip);
-//console.log(student1.music[1]); display beatles
-
 let salon = {
     name:"The Fashion Pet",
     address:{
@@ -28,51 +7,66 @@ let salon = {
     hours:{
     open:"9:00 am",
     close:"8:00 pm"
-    }
+    },
+    pets:[]
 }
+
+//object constructor
+function Pet(name,age,gender,breed,service,type){
+    this.petName=name;
+    this.petAge=age;
+    this.petGender=gender;
+    this.petBreed=breed;
+    this.petService-service;
+    this.petAge.Type=type;
+}
+//global
+let inputName= document.getElementById("txtName");
+let inputAge= document.getElementById("txtAge");
+let inputGender= document.getElementById("txtGender");
+let inputBreed= document.getElementById("txtBreed");
+let inputService= document.getElementById("txtService");
+let inputType= document.getElementById("txtType");
+
 // create the function
-function displayInfo(){
-document.getElementById("salon").innerHTML=`Welcome to ${salon.name}.....close at ${salon.hours.close}`;
-}
-//call/run/execute the function
+function register(){
+//console.log("Registering");
+//get the values from the form
 
-displayInfo();
-let pet1= {
-        name:"Scooby",
-        age:"12",
-        gender:"Male",
-        service: "Shampoo and Cut",
-        breed: "German Sheperd" 
+//console.log(inputName,inputAge,inputGender,inputBreed,inputService,inputType);
+
+//create the newPet object
+
+let newPet = new Pet(inputName.value,
+    inputAge.value,
+    inputGender.value,
+    inputBreed.value,
+    inputService.value,
+    inputType.value);
+
+console.log(newPet);
+salon.pets.push(newPet);
+clearInput();
 }
+
+function clearInput() {
+inputName.value="";
+inputAge.value="";
+inputGender.value="";
+inputBreed.value="";
+inputService.value="";
+inputType.value="";
+
+}
+function init(){
+    let pet1 = new Pet("Scooby", 10,"Male", "Pit Bull");
+    let pet2 = new Pet("Scrappy", 8, "Female", "Dobberman");
+    let pet3 = new Pet("Happy", 12, "Male", "French Bulldog");
+    let pet4 = new Pet("Lady", 3, "Female", "Dane");
     
-let pet2 =  {
-    name:"Scappy",
-    age:"8",
-    gender:"Female",
-    service:"Nail Clipping",
-    breed:"French Bull Dog",
-    }
-let pet3 = { 
-    name:"Happy",
-    age:"17",
-    gender:"Male",
-    service: "Shampoo, Nail Clipper and Cut",
-    breed: "Pit Bull"
-}
-const pets= [pet1,pet2,pet3];
-
-
-
-
-function displayPetInfo(pet){
-document.getElementById("pet1").innerHTML+=`<p>${pet.name}</p>`;
+    salon.pets.push(pet1,pet2,pet3,pet4);
+   // console.log(salon.pets);
 }
 
-displayPetInfo(pet1);
-displayPetInfo(pet2);
-displayPetInfo(pet3);
-
-
-
-
-
+window.onload=init;//wait to render the html to execute init
+//displayInfo();
